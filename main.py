9db4453a3f44aabc1058129d1ba0b20a574acb08
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2022-03-31 23:02:23 +0300

    update first commit

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import pandas as pd
+from pars_site import Pars_site
 
-URL = "https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html"
-r = requests.get(URL)
-#print(r.status_code)
-#print(r.text)
+if __name__ == '__main__':
+    URL = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
 
-soup = bs(r.text, 'html.parser')
-#print(soup)
-html_soup = soup.find_all('div', id = 'PageContent')
-
-#print(html_soup[0].text)
-str_soup = list(set([str.lower(i) for i in html_soup[0].text.split(' ') if i.isalpha(): i = ' ']))
-
-print(len(html_soup[0].text))
-#str_soup = filter(str.isalpha, html_soup[0].text)
-#str_soup = ' '.join(str_soup)
-#for i in str_soup:
-#    print(i, end = '\n')
-print(len(str_soup))
-
-
+    print(Pars_site(URL))
