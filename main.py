e8e3bb62b0d8d29ad0c62f259dc5a207014a88a7
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2022-04-01 19:17:00 +0300

    add requirements.tst

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from pars_site import Pars_site
 
 if __name__ == '__main__':
     URL = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
-
+    #URL = 'https://realpython.com/dockerizing-flask-with-compose-and-machine-from-localhost-to-the-cloud/'
+    t = Pars_site(URL)
     print(Pars_site(URL))
+    print(len(t))
