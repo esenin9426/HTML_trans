c0c704d3375c272809e43258f5a5e3fe8bdab70a
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2022-04-01 19:05:31 +0300

    update gitignore pars_store

--- pars_site.py
+++ pars_site.py
@@ -4,9 +4,20 @@ def Pars_site(URL):
     r = requests.get(URL)
     soup = bs(r.text, 'html.parser')
 
-    html_soup = soup.find_all('div', id = 'PageContent')
-    html_str = html_soup[0].text
-    html_str = ''.join([i.lower() for i in html_str if  i.isalpha() or i == ' ']).split(' ')
+    html_soup = soup.find_all('div')
+
+    html_str = ''
+
+    for i in html_soup:
+        html_str += i.text
+    html_str = ''.join([i.lower() for i in html_str if i.isalpha() or i == ' ']).split(' ')
     html_str = list(set(html_str))
+    html_str.remove('')
 
     return html_str
+
+
+if __name__ == '__main__':
+    URL = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
+
+    print(Pars_site(URL))
