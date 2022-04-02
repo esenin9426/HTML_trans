4f9dd5d010956738f4be04193ac1bdfbe4754e05
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2022-04-02 11:44:26 +0300

    show file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pars_site import Pars_site
 
 if __name__ == '__main__':
-    URL = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
-    #URL = 'https://realpython.com/dockerizing-flask-with-compose-and-machine-from-localhost-to-the-cloud/'
-    t = Pars_site(URL)
-    print(Pars_site(URL))
-    print(len(t))
+    URL1 = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
+    URL2 = 'https://realpython.com/dockerizing-flask-with-compose-and-machine-from-localhost-to-the-cloud/'
+    t = Pars_site(URL1)
+    t2 = Pars_site(URL2)
+    print(t)
+    print(t2)
