85c467cef09e65103278a748b0cdd0716202e738
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2022-04-05 21:32:51 +0300

    done HP-4

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pars_site import Pars_site
+from translate import Translator
 
 if __name__ == '__main__':
     URL1 = 'https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html'
-    URL2 = 'https://realpython.com/dockerizing-flask-with-compose-and-machine-from-localhost-to-the-cloud/'
     t = Pars_site(URL1)
-    t2 = Pars_site(URL2)
-    print(t)
-    print(t2)
+    translator = Translator()
+
+    for i in t[:10]:
+        print(i + ' : ' + translator.translate(i))
